807199f9473463f88015880f427fd03ef3cdd082
Kenta <kenta@Kentas-MacBook-Pro.local>
2026-06-18 16:44:08 +0900

    fix:completed_button

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -30,5 +30,10 @@ class TasksController < ApplicationController
                               @selected_date
                             )
                             .order(:test_end_date)
+
+      completed_task_ids = current_user.task_completions.select(:task_id)
+      @incomplete_tasks = Task
+                          .where(homework_id: @homeworks.ids)
+                          .where.not(id: completed_task_ids)
   end
 end
