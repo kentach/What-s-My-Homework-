6cb924d1db029ad2c6ac7c76ff1da6a8b5781b1a
Kenta <kenta@Kentas-MacBook-Pro.local>
2026-06-19 10:53:09 +0900

    fix:UI_task_complete_index

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,10 +4,8 @@ class TasksController < ApplicationController
   def index
     base_date = params[:week].present? ? Date.parse(params[:week]) : Date.today
     # 値が入っていればその日付を使い、入っていなければ今日の日付を使う
-
     week_start = base_date.beginning_of_week(:monday) # beginning_of_week = 週の開始日を返すメソッド
     week_end   = week_start + 6.days
-
     @week_days = (week_start..week_end).to_a
     @prev_week = week_start - 7.days
     @next_week = week_start + 7.days
