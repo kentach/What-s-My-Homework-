adf0aefbf30fbb0ae12d65656bfc9ff074bafe3d
Kenta <kenta@Kentas-MacBook-Pro.local>
2026-06-25 20:37:06 +0900

    fix

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,6 @@
 class Admin::DashboardController < Admin::BaseController
   def index
+    @homeworks = Homework.order(created_at: :desc)
     @published_homeworks = Homework.published
     @draft_homeworks = Homework.draft
     @classrooms = Classroom.order(created_at: :asc)
