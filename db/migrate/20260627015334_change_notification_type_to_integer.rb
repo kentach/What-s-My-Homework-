de1f9f9b98b1ca71d5a2d777e12fbf1f9118bb74
Kenta <kenta@Kentas-MacBook-Pro.local>
2026-06-27 11:05:04 +0900

    fix:rubocop

--- db/migrate/20260627015334_change_notification_type_to_integer.rb
+++ db/migrate/20260627015334_change_notification_type_to_integer.rb
@@ -5,7 +5,7 @@ class ChangeNotificationTypeToInteger < ActiveRecord::Migration[7.2]
     change_column :notifications, :target_type, :integer,
       using: "target_type::integer", default: 0, null: false
   end
-  
+
   def down
     change_column :notifications, :notification_type, :string,
       default: "0", null: false
